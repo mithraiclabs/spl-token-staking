02ffdf2fab52e5dea0c219a16aa29de4350d6117
Taylor123 <nftaylor11@gmail.com>
2023-09-25 13:12:59 -0700

    add batch request stake receipts. Export types

--- packages/token-staking/src/index.ts
+++ packages/token-staking/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./constants";
 export * from "./idl";
 export * from "./instructions";
+export * from "./types";
 export * from "./utils";
--- packages/token-staking/src/types.ts
+++ packages/token-staking/src/types.ts
@@ -1,6 +1,9 @@
-import { IdlAccounts } from "@coral-xyz/anchor";
+import { IdlAccounts, web3 } from "@coral-xyz/anchor";
 import { SplTokenStaking } from "./idl";
 
 export type StakePool = IdlAccounts<SplTokenStaking>["stakePool"];
 export type StakeDepositReceipt =
   IdlAccounts<SplTokenStaking>["stakeDepositReceipt"];
+export type StakeDepositReceiptData = StakeDepositReceipt & {
+  address: web3.PublicKey;
+};
