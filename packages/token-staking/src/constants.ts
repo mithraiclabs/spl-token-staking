9985745a4dece676b7cb74f424a55067c2110cb5
Taylor123 <nftaylor11@gmail.com>
2023-09-25 15:57:51 -0700

    add TS utils for calculating stake weight and remaining accounts

--- packages/token-staking/src/constants.ts
+++ packages/token-staking/src/constants.ts
@@ -1,4 +1,7 @@
+import * as anchor from "@coral-xyz/anchor";
+
 export const SCALE_FACTOR_BASE = 1_000_000_000n;
+export const SCALE_FACTOR_BASE_BN = new anchor.BN(1_000_000_000);
 export const U64_MAX = 18446744073709551615n;
 
 export const SPL_TOKEN_STAKING_DEVNET_ID =
