34f943ba6b62285ca655bd751c0954887be3af7a
Taylor123 <nftaylor11@gmail.com>
2023-10-25 16:58:41 -0400

    bump package

--- packages/token-staking/src/constants.ts
+++ packages/token-staking/src/constants.ts
@@ -4,5 +4,11 @@ export const SCALE_FACTOR_BASE = 1_000_000_000n;
 export const SCALE_FACTOR_BASE_BN = new anchor.BN(1_000_000_000);
 export const U64_MAX = 18446744073709551615n;
 
-export const SPL_TOKEN_STAKING_DEVNET_ID =
+export const SPL_TOKEN_STAKING_ID =
   "STAKEkKzbdeKkqzKpLkNQD3SUuLgshDKCD7U8duxAbB";
+
+/**
+ * @deprecated
+ * Use `SPL_TOKEN_STAKING_ID` instead
+ */
+export const SPL_TOKEN_STAKING_DEVNET_ID = SPL_TOKEN_STAKING_ID;
